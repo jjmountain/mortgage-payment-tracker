1d18ca9cdbb98af9de4bc9bc4cbbb258219a587b
James Devereux <devereuxjj@gmail.com>
2025-03-30 21:53:05 +0100

    use hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { MortgageCalculator } from './components/MortgageCalculator';
 import { PaymentTracker } from './components/PaymentTracker';
 import { ExpenseTracker } from './components/ExpenseTracker';
